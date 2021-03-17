213c22ae7b3ac7898b1a03c1d577328a983b8ff7
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-03-16 19:45:52 -0500

    Update align_reads_using_STAR.py

--- align_reads_using_STAR.py
+++ align_reads_using_STAR.py
@@ -70,8 +70,8 @@ def mapSamplesToReference(options):
                 cmd+=" --alignIntronMax 1 "
             if os.path.exists(options.output_directory+"/"+sra+"_"+str(iteration)+"_Log.final.out")==False:
                 list_of_all_commands.append([cmd,"dummy"])
-    
-    print(list_of_all_commands)
+    for row in list_of_all_commands:
+        print(row[0])
     return
     pool.map(runCommand,list_of_all_commands)
     
