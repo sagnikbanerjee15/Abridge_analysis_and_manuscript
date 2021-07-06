ac8bd686ff8b9164a9843941506d15feedd0d8ec
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-07-05 19:16:34 -0500

    Update extensively_test_abridge.py

--- extensively_test_abridge.py
+++ extensively_test_abridge.py
@@ -95,7 +95,7 @@ for level in ["1","2","3"]: # 3 iterations
                                         cmd += f" 2> {output_directory_name}.error "
                                         output_directory_name_without_location = output_directory_name.split("/")[-1]
                                         if os.path.exists(f"{output_directory_name}/{inputfilename_without_location}.abridge")==False and os.path.exists(f"{TEMP_DIRECTORY}/{output_directory_name_without_location}/{inputfilename_without_location}.abridge") == False:
-                                            cmd_mv = f"mv {output_directory_name} {TEMP_DIRECTORY}"
+                                            cmd_mv = f"mv {output_directory_name}* {TEMP_DIRECTORY}"
                                             compress_commands.append([cmd,cmd_mv])
                                         #print(f"{output_directory_name}/{inputfilename_without_location}.abridge")
                                         #print(f"{TEMP_DIRECTORY}/{output_directory_name_without_location}/{inputfilename_without_location}.abridge")
