a532c6a363cad1497d373968ada5ae4c90e9c51f
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-09-09 07:45:46 -0500

    Update align_reads_using_STAR_and_BWA.py

--- align_reads_using_STAR_and_BWA.py
+++ align_reads_using_STAR_and_BWA.py
@@ -67,19 +67,19 @@ def mapSamplesToReference(options):
                 cmd  = f" cp "
                 cmd += f" {options.temp_raw_data_directory}/{sra}_1.fastq "
                 cmd += f" {options.input_location}/ "
-                if os.path.exists(f"{options.input_location}/{sra}_1.fastq")==False:
+                if os.path.exists(f"{options.input_location}/{sra}_1.fastq")==False and os.path.exists(f"{options.temp_alignments_directory}/{sra}_{layout}.bam")==False and os.path.exists(f"{options.temp_alignments_directory}/{sra}_{layout}.sam")==False:
                     os.system(cmd)
                 
                 cmd  = f" cp "
                 cmd += f" {options.temp_raw_data_directory}/{sra}_2.fastq "
                 cmd += f" {options.input_location}/ "
-                if os.path.exists(f"{options.input_location}/{sra}_2.fastq")==False:
+                if os.path.exists(f"{options.input_location}/{sra}_2.fastq")==False and os.path.exists(f"{options.temp_alignments_directory}/{sra}_{layout}.bam")==False and os.path.exists(f"{options.temp_alignments_directory}/{sra}_{layout}.sam")==False:
                     os.system(cmd)
             else:
                 cmd  = f" cp "
                 cmd += f" {options.temp_raw_data_directory}/{sra}_0.fastq "
                 cmd += f" {options.input_location}/ "
-                if os.path.exists(f"{options.input_location}/{sra}_0.fastq")==False:
+                if os.path.exists(f"{options.input_location}/{sra}_0.fastq")==False and os.path.exists(f"{options.temp_alignments_directory}/{sra}_{layout}.bam")==False and os.path.exists(f"{options.temp_alignments_directory}/{sra}_{layout}.sam")==False:
                     os.system(cmd)
             
             files_to_be_removed=[]
